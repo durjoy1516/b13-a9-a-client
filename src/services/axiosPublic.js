ea21da239af753c2cc9ec7845e866ca9cf5f654e
durjoy1516 <durjoy0184@gmail.com>
2026-08-03 23:21:54 +0600

    Update baseURL with backend server URL

--- src/services/axiosPublic.js
+++ src/services/axiosPublic.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosPublic = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://findouttutors.vercel.app',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://b13-a9-server-exbtz0gy4-durjoy1516s-projects.vercel.app',
 });
 
 export default axiosPublic;
